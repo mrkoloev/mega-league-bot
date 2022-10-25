ec7b34293b945c39d4e364e427927e8e26af6406
mrkoloev <42695638+mrkoloev@users.noreply.github.com>
2022-10-25 23:22:17 +0300

    added envs import

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 import telegram_bot
-from config import DISCORD_TOKEN, TG_TOKEN, TG_CHAT_ID
+import os
 import parser
 
 intents = discord.Intents.default()
@@ -25,11 +25,12 @@ async def on_message(message, game_url=None):
         game_url=msg[msg.find('http'):]
         parser.download_recap_img(game_url)
         img_path = game_url[game_url.find("gamerecap"):] + '.png'
-        await telegram_bot.send_img(img_path=img_path, chat_id=TG_CHAT_ID, token=TG_TOKEN, url=game_url)
+        await telegram_bot.send_img(img_path=img_path, chat_id=os.environ.get('TG_CHAT_ID'), token=os.environ.get('TG_TOKEN'), url=game_url)
 
     if 'league' in message.content:
         # print(type(message.content))
         await telegram_bot.send_msg(msg=message.content, chat_id=TG_CHAT_ID, token=TG_TOKEN)
         # await message.channel.send("JJJ")
 
-client.run(token=DISCORD_TOKEN)
+if __name__ == '__main__':
+    client.run(token=os.environ.get('DISCORD_TOKEN'))
