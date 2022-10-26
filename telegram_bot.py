27c151c0f8e5bba1ca7ed2314dfc0000a826bb85
mkoloev <mkoloev@gpmdata.ru>
2022-10-26 23:56:18 +0300

    Merge remote-tracking branch 'origin/main'

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,16 +1,16 @@
-import telegram
+import telegram as tg
 
 
 def send_msg(msg, chat_id: str, token: str, parse_mode='HTML'):
-    bot = telegram.Bot(token=token)
+    bot = tg.Bot(token=token)
     bot.sendMessage(chat_id=chat_id, text=msg, parse_mode=parse_mode, disable_web_page_preview=True)
 
 
 def send_document(document, chat_id: str, token: str):
-    bot = telegram.Bot(token=token)
+    bot = tg.Bot(token=token)
     bot.send_document(chat_id=chat_id, document=document)
 
 
 def send_img(img_path, chat_id: str, token: str, url):
-    bot = telegram.Bot(token=token)
+    bot = tg.Bot(token=token)
     bot.send_photo(photo=open(file=img_path, mode='rb'), chat_id=chat_id, caption=url)
