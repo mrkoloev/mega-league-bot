966b992cb4a44216acd99e2e7a640b4c481195f2
mkoloev <mkoloev@gpmdata.ru>
2022-10-29 00:37:09 +0300

    Merge remote-tracking branch 'origin/main'

--- parser.py
+++ parser.py
@@ -1,21 +1,25 @@
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.service import Service as ChromeService
+
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.edge.service import Service as EdgeService
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import os
-from selenium.webdriver.chrome.service import Service as ChromeService
 
 def download_recap_img(url: str):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    browser_options = Options()
+    browser_options.add_argument("--headless")
 
     prefs = {"download.default_directory": "./gamerecap"}
-    chrome_options.add_experimental_option("prefs", prefs)
-    # browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-    service = ChromeService(executable_path=ChromeDriverManager().install())
+    browser_options.add_experimental_option("prefs", prefs)
+    service = EdgeService(executable_path=EdgeChromiumDriverManager().install())
     print('service initialized')
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Edge(service=service, options=browser_options)
     print('driver initialized')
     driver.set_window_size(1400, 800)
     driver.get(url)
